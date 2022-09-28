cdd472430fc81691d0d0bb2689b7b2e05913c97b
b19wilfo <62884368+b19wilfo@users.noreply.github.com>
2022-09-28 07:15:00 +0200

    Update index.php
    "Include "Redis.php";

--- index.php
+++ index.php
@@ -11,25 +11,27 @@
       <input type="text" name="searchbar" id="searchbar" value="" />
       <button type="submit" name="searchbt" id="searchbt">
     </form>
-    <div id="sparning"> <?php
-
-  ?> <script>
+    <div id="sparning"> 
+      <?php
+        include "Redis-php";
+      ?> 
+       <script>
         //Saves responsetimes to localstorage
-        var searchInput = document.getElementById("q");
+        /*var searchInput = document.getElementById("q");
         if (iteration > times) {
           running = false;
           localStorage.setItem('search_running', false);
           console.log(JSON.parse(localStorage.getItem('search_result')))
           var arr = new Array();
           JSON.parse(localStorage.getItem('search_result')).forEach(x => {
-                arr.push(x['time']);
+                arr.push(x['time']);*/
       </script>
     </div>
     <div id="publiserare">
       <form method="post"> Rubrik: <input type="text" id="rubrikinput" placeholder="Rubrik">
         <br> Underrubrik <input type="text" id="underrubrikinput" placeholder="Underrubrik">
         <br> Brödtext <input type="text" id="textinput" placeholder="Brödtext">
-        <br> Författare <input type="text" id="forfattareinput" placeholder="Förafattare">
+        <br> Författare <input type="text" id="forfattareinput" placeholder="Författare">
         <br>
         <button id="Publisera" type="submit" name="publisera">Publisera</button>
         <button id="Radera" type="submit" name="radera">Radera</button>
